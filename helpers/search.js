7b1bb827b20025878154a1b2102c1c959f887b82
Kevin van Dijk <kevin@edgybits.com>
2016-04-18 00:29:03 +0200

    Re-enable wordcount search for fuzzy movie search

--- helpers/search.js
+++ helpers/search.js
@@ -1,6 +1,6 @@
 import FuzzySearch from 'fuzzysearch-js';
 import levenshteinFS from 'fuzzysearch-js/js/modules/LevenshteinFS';
-// import wordCountFS from 'fuzzysearch-js/js/modules/WordCountFS';
+import wordCountFS from 'fuzzysearch-js/js/modules/WordCountFS';
 import indexOfFS from 'fuzzysearch-js/js/modules/IndexOfFS';
 
 
@@ -17,7 +17,7 @@ export function fuzzySearch(list, searchValue, options) {
   try {
     const fuzzySearch = new FuzzySearch(list, options);
     fuzzySearch.addModule(levenshteinFS({'maxDistanceTolerance': 10, 'factor': 3}));
-    // fuzzySearch.addModule(wordCountFS({'maxWordTolerance': 3, 'factor': 1}));
+    fuzzySearch.addModule(wordCountFS({'maxWordTolerance': 3, 'factor': 1}));
     fuzzySearch.addModule(indexOfFS({'minTermLength': 3, 'maxIterations': 500, 'factor': 3}));
 
 
