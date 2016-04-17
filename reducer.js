40c191235805421917bdc4f39a3a652c5ca49c4e
Kevin van Dijk <kevin@edgybits.com>
2016-04-18 01:48:37 +0200

    Create function to wait for a specified action to have been reduced
    This function can be used to await a specific moment to show the current state

--- reducer.js
+++ reducer.js
@@ -1,6 +1,24 @@
+import Promise from 'bluebird';
 import { combineReducers } from 'redux';
 import plexReducer from './channels/plex/reducer';
 
-export default combineReducers({
+const reducer = combineReducers({
   plex: plexReducer,
 });
+
+export default reducer;
+
+export function waitOnAction(store, actionType) {
+  return new Promise(resolve => {
+    store.replaceReducer((state, action) => {
+      const reduced = reducer(state, action);
+
+      if (action.type === actionType) {
+        resolve(reduced);
+      }
+      return reduced;
+    });
+  }).finally(() => {
+    store.replaceReducer(reducer);
+  });
+}
